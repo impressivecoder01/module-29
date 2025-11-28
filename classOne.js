8f27f2919bee5a3e58d1a3db21530c2f4db78d57
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-29 00:50:52 +0600

    class one done

--- classOne.js
+++ classOne.js
@@ -2,3 +2,11 @@
 const nam = 'bangladesh';
 const nowName = 'bd' + ' '+ nam
 console.log(nowName)
+let price = 50;
+price = 100;
+console.log(price)
+const dress = ['shirt', 'pant', 't-shirt'];
+dress.push('a');
+console.log(dress)
+
+// DIFFERENT BETWEEN VAR, LET AND CONST-----> use Chatgpt
