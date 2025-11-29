0c27cbfaf5d75eb5981f22e19683954547b10f1f
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-29 22:33:16 +0600

    classfive

---
+++ classfive.js
@@ -0,0 +1,19 @@
+// more arrow functions and arrow functions =>
+const getTax = (amount, taxRate) => amount * taxRate/ 100;
+console.log(getTax(400000, 5))
+const getSquare = (x) => x*x;
+console.log(getSquare(4))
+const getHalf = num => num/2;
+console.log(getHalf(10))
+const firstElement = nums => nums[0];
+console.log(firstElement([23,33,456,6]))
+const log =()=> console.log('consoled')
+log()
+
+// annymous function
+document.getElementById('').addEventListener('click', ()=> {
+
+})
+document.getElementById('').addEventListener('click', event=>{
+    
+})
