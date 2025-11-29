f84feac715ebe1a9cde0cc277617b14208e1c417
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-30 01:08:57 +0600

    class 8

---
+++ classEight.js
@@ -0,0 +1,9 @@
+// keys, values, entries, delete, seal, freeze
+const employee = {
+    name: 'rana', 
+    designation: 'dev',
+    salary: 20000,
+    experience: 1
+}
+const keys = Object.keys(employee)
+console.log(keys)
