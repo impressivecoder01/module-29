30802b81f1fb4f232997c4c9051bd0ccf9fdec6e
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-30 01:36:14 +0600

    class eight added

--- classEight.js
+++ classEight.js
@@ -3,7 +3,18 @@ const employee = {
     name: 'rana', 
     designation: 'dev',
     salary: 20000,
-    experience: 1
+    experience: 1,
+    age: 22
 }
 const keys = Object.keys(employee)
 console.log(keys)
+console.log(Object.values(employee))
+const entries = Object.entries(employee)
+console.log(entries)
+console.log(employee)
+employee.experience = employee.experience +1
+// Object.freeze(employee)
+Object.seal(employee)
+employee.location = 'Dhaka'
+// delete employee.experience;
+console.log(employee)
